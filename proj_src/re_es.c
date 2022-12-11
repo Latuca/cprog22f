bb6dee42168624a81dc0a1ea9996f73543b66a4e
Latuca <ychan0302@naver.com>
2022-12-12 00:14:52 +0900

    commit message

---
+++ proj_src/re_es.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+typedef struct real_estate{
+    char address[20];
+    char apt_name[20];
+    int price;
+}Re_Es;
+int main()
+{
+    Re_Es A;
+    return 0;
+}
