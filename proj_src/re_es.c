d77dcc769022a66cb4258d796f7b8754d317594d
Latuca <ychan0302@naver.com>
2022-12-13 02:10:19 +0900

    commit message

--- proj_src/re_es.c
+++ proj_src/re_es.c
@@ -4,7 +4,7 @@
 #include <stdlib.h>
 
 #define ESC 0x1b
-//1.키보드 입력 감지 getch함수 구현
+//키보드 입력 감지 getch함수 구현
 #include <termio.h>
 
 int getch(void)
@@ -30,18 +30,18 @@ int getch(void)
 // 여기서부터 시작
 typedef struct node{
     char address[20];
-    char apt_name[20];
-    float space;
+    char apt_name[30];
+    int space;
     long int price;
     struct node *next;
 }Node;
-//2. 검색기능 구현
+//1. 검색기능 구현
 Node *search_node(char *apt_name, Node* list_head)
 {
     Node *tmp=list_head;
     while(tmp){
         if(!strcmp(tmp->apt_name,apt_name)){ //검색할 단지명 찾음.
-            return tmp;
+            return tmp; //해당 단지 정보 리턴
         }
         tmp=tmp->next;  //다음 노드로 이동하여 검색진행
     }
@@ -55,8 +55,8 @@ void main()
     Node *tmp_node;
     Node *list_head=NULL;
     char address[20];
-    char apt_name[20];
-    float space;
+    char apt_name[30];
+    int space;
     long int price;
     char input ='0';
 
@@ -66,23 +66,23 @@ void main()
         printf("Cannot open file\n");
         return;
     }
-    // 학생정보읽기
+    // 부동산정보읽기
     while(!feof(fp)){
         new_node=(Node*)malloc(sizeof(Node)); //동적 메모리 할당
-        fscanf(fp,"%s %s %f %ld\n",&new_node->address,&new_node->apt_name,&new_node->space,&new_node->price);
-        printf("노드 추가: %s %s %f %ld\n",&new_node->address,&new_node->apt_name,&new_node->space,&new_node->price);
+        fscanf(fp,"%s %s %d %ld ",new_node->address,new_node->apt_name,&new_node->space,&new_node->price);
+        printf("\n[노드 추가]:\n %s %s %d %ld",new_node->address,new_node->apt_name,new_node->space,new_node->price);
         // 노드추가
         new_node->next=list_head;
         list_head=new_node;
     }
-        // 포트폴리오 내역 조회
+        //부동산 정보 검색
         while(input!=ESC){
-            printf("------------------------------\n");
-            printf("부동산 정보 조회(시군구_단지명):");
-            scanf("%s",&apt_name);
+            printf("\n------------------------------\n");
+            printf("부동산 정보 검색(단지명):");
+            scanf("%s",apt_name);
             tmp_node=search_node(apt_name,list_head);
             if(tmp_node)
-                printf("%s %s %f %ld\n",tmp_node->address,tmp_node->apt_name,tmp_node->space,tmp_node->price);
+                printf("%s %s %d %ld\n",tmp_node->address,tmp_node->apt_name,tmp_node->space,tmp_node->price);
             else
                 printf("찾으시는 매물이 없습니다.\n");
             printf("종료는 ESC, 계속은 Enter 키를 누르세요\n");
@@ -96,7 +96,7 @@ void main()
         }
         fclose(fp);
 }
-
+void
 
 
 
